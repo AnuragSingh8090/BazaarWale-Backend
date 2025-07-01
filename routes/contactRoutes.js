417cd870b261ce1d2ae05d4985fa13588ace6160
Anurag1EQ <anurag.singh@1eq.in>
2025-07-01 10:29:48 +0530

    Remove all files from Git tracking

--- routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { contactUs } from '../controllers/contactControllers.js';
-
-const contactRouter = Router();
-
-contactRouter.post('/contact', contactUs);
-
-export default contactRouter;
